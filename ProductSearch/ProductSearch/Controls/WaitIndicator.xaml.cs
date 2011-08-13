a79c8945168af29a7709725ddbf10fd0d20cb6fe
Eric Scherrer <escherrer@gmail.com>
2011-08-13 00:43:31 -0400

    added wait indicator

---
+++ ProductSearch/ProductSearch/Controls/WaitIndicator.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ProductSearch.Controls
+{
+    /// <summary>
+    /// Interaction logic for WaitIndicator.xaml
+    /// </summary>
+    public partial class WaitIndicator : UserControl
+    {
+        public WaitIndicator()
+        {
+            InitializeComponent();
+        }
+    }
+}
