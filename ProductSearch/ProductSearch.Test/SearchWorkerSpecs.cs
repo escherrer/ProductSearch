54fcc949be598a634bad737ebf20f175a37f88a3
Eric Scherrer <escherrer@gmail.com>
2011-08-13 14:01:44 -0400

    added intial search engine specs

---
+++ ProductSearch/ProductSearch.Test/SearchWorkerSpecs.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProductSearch.Test
+{
+    [TestClass]
+    public class SearchWorkerSpecs
+    {
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+
+        }
+
+        [TestMethod]
+        public void When_a_search_finishes_successfully_it_should_return_success_results()
+        {
+
+        }
+
+        [TestMethod]
+        public void When_a_search_fails_it_should_return_failure_results()
+        {
+
+        }
+
+        [TestMethod]
+        public void When_a_search_is_cancelled_it_should_return_failure_results()
+        {
+
+        }
+    }
+}
