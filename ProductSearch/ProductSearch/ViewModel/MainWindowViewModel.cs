3b2ad0668b7b5f47a9ab65ce8bf54c4997eef76a
Eric Scherrer <escherrer@gmail.com>
2011-08-13 13:28:17 -0400

    more UI stuffs

--- ProductSearch/ProductSearch/ViewModel/MainWindowViewModel.cs
+++ ProductSearch/ProductSearch/ViewModel/MainWindowViewModel.cs
@@ -1,48 +1,44 @@
 ﻿using System;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
-using GalaSoft.MvvmLight.Messaging;
 using ProductSearch.Utility;
 
 namespace ProductSearch.ViewModel
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        private bool _isSearching;
-
         public MainWindowViewModel()
         {
             SearchCommand = new RelayCommand<string>(DoSearch);
+
+            // Test
+            ProductImage = new Uri("http://images.bestbuy.com/BestBuy_US/images/products/9321/9321245_sc.jpg");
+            ErrorMessage = "* Unable to blah blah blah.";
+            ProductPrice = 25.55m;
+            IsSearching = true;
         }
 
-        public decimal? ProductPrice
-        {
-            get { return 25.55m; }
-        }
+        public decimal? ProductPrice { get; set; }
 
-        public Uri ProductImage
-        {
-            get
-            {
-                return new Uri("http://images.bestbuy.com/BestBuy_US/images/products/9321/9321245_sc.jpg");
-            }
-        }
+        public Uri ProductImage { get; set; }
 
-        public bool IsSearching
-        {
-            get { return _isSearching; }
-            set
-            {
-                _isSearching = value; 
-                RaisePropertyChanged(ReflectionHelper.GetPropertyName(() => IsSearching));
-            }
-        }
+        public bool IsSearching { get; set; }
+
+        public string ErrorMessage { get; set; }
 
         public RelayCommand<string> SearchCommand { get; private set; }
 
         private void DoSearch(string productName)
         {
 
+        }
+
+        private void RefreshUi()
+        {
+            RaisePropertyChanged(ReflectionHelper.GetPropertyName(() => IsSearching));
+            RaisePropertyChanged(ReflectionHelper.GetPropertyName(() => ProductImage));
+            RaisePropertyChanged(ReflectionHelper.GetPropertyName(() => ProductPrice));
+            RaisePropertyChanged(ReflectionHelper.GetPropertyName(() => ErrorMessage));
         }
     }
 }
